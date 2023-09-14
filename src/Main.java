0a68dd1f805b96d44a0fb0b1236cebd41e2c799d
fitz3 <fitz3kat@gmail.com>
2023-09-14 13:43:48 -0400

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        // intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+        int intOperandA =  1;
+        int intOperandB = 6;
+
+        int intSum = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        // rest of the int statements go here for product, difference, quotient, modulo
+
+        double doubleOperandA = 24.6;
+        double doubleOperandB = 6.5;
+        double doubleSum = doubleOperandA + doubleOperandB;
+
+        System.out.println("the sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum );
+
+        // variable
+        int numKids = 5;
+        boolean isRaining = false;
+        double pricePerGallon = 4.25;
     }
 }
